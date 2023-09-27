26d4a1342ece75ceb33018ff911b7529c30ed140
gprielipp2026 <m265112@usna.edu>
2023-09-27 08:25:50 -0400

    finished

--- p1.cpp
+++ p1.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
 
+string var(string pre, int idx) {return pre + to_string(idx);}
+
 int main()
 {
+    char junk;
+    // read n
+    int n;
+    cin >> junk >> junk >> n;
+    // read n ints
+    int vals[n] = {0};
+    for(int i = 0; i < n; i++)
+        cin >> vals[i];
+    // read W
+    int W;
+    cin >> junk >> junk >> W;
+
+    // output a's
+    for(int i = 0; i < n; i++)
+        cout << "(" << var("a", i) << " => " << var("w", i) << " = " << vals[i] << ") & ("
+             << var("~a", i) << " => " << var("w", i) << " = 0) &" << endl;
+
+    // output the sums
+    cout << "sum = ";
+    for(int i = 0; i < n-1; i++)
+        cout << "plus(";
+    cout << "w0,";
+    for(int i = 1; i < n-1; i++)
+        cout << var("w", i) << "),";
+    cout << var("w", n-1) << ") &" << endl;
+    cout << "sum = " << W << endl;
+
+
     return 0;
 }
